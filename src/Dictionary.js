bad226c428b20b84c5ea957ae1b4495982eeba17
MYO THIRI <sandarlamin.llm@gmail.com>
2021-09-11 14:01:03 +0900

    Added dictionary api integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,17 +1,27 @@
 import React, {useState} from "react";
 import "./Dictionary.css";
+import axios from "axios";
 
 export default function Dictionary(){
     let [keyword, setKeyword] = useState("");
     function handleSubmit(event){
         event.preventDefault();
-        alert(`Searching for ${keyword}`); 
+        // alert(`Searching for ${keyword}`); 
+        //documentation: https://dictionaryapi.dev/
+        const apiUrl = "https://api.dictionaryapi.dev/api/v2/entries/en";
+        axios
+        .get(`${apiUrl}/${keyword}`)
+        .then(handleResponse);
     }
 
     function handleSearch(event){
         event.preventDefault();
-        console.log(event.target.value);
+        // console.log(event.target.value);
         setKeyword(event.target.value);
+    }
+
+    function handleResponse(response){
+        console.log(response.data[0]);
     }
     return(
         <div className="Dictionary">
